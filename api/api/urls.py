589c7f88e1cf9a4e16f88606d083bda24d6b3069
Роман Попов <i@romanpopoff.ru>
2022-12-17 12:01:40 +0300

    feat: modelviewset

--- api/api/urls.py
+++ api/api/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from core.views import QuestionAPIView
+from core.views import QuestionViewSet
+
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'questions', QuestionViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/questionlist/', QuestionAPIView.as_view()),
-    path('api/v1/questionlist/<int:pk>/', QuestionAPIView.as_view()),
+    path('api/v1/', include(router.urls)),
 ]
