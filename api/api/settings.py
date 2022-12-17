fcc21eb4c6614c75cebf6762e196f28bc7dbafad
Роман Попов <i@romanpopoff.ru>
2022-12-17 15:26:24 +0300

    pagination

--- api/api/settings.py
+++ api/api/settings.py
@@ -128,6 +128,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 1,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
--- api/core/views.py
+++ api/core/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from core.models import Question
@@ -6,10 +7,16 @@ from core.permissions import IsOwnerOrReadOnly, IsAdminOrReadOnly
 from core.serializers import QuestionSerialazer
 
 
+class QuestionsPagList(PageNumberPagination):
+    page_size = 4
+    page_query_param = 'page_size'
+    max_page_size = 1000
+
 class QuestionAPIList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerialazer
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    pagination_class = QuestionsPagList
 
 
 class QuestionAPIUpdate(generics.RetrieveUpdateAPIView):
