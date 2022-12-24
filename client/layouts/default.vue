2aaa87405ced7ec1d67211638b6f17b7d81c75df
Роман Попов <i@romanpopoff.ru>
2022-12-24 18:00:26 +0300

    try connect to api

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Navbar />
-    <nuxt />
+      <Nuxt />
     <Footer />
   </div>
 </template>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,39 +1,25 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        client
-      </h1>
-      <h2 class="subtitle">
-        My dazzling Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <h1>Список тестов</h1>
+    <div v-for="question in questions" :key="question.title">
+      <div>{{question.title}}</div>
     </div>
   </div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      questions: []
+    }
+  },
+  async fetch() {
+    this.questions = await fetch(
+      'http://127.0.0.1:8000/api/v1/questions/'
+    ).then(res => res.json())
+    console.log(this.questions.result)
   }
 }
 </script>
