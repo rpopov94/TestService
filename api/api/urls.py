e74590b1140699c8db79230630578fa40d94338c
rpopov <rpopov@web-bee.ru>
2023-01-06 14:11:29 +0200

    регистрация

--- api/api/urls.py
+++ api/api/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-from core.views import QuestionAPIUpdate, QuestionAPIDestroy, ThemeAPIList, ThemeAPIUpdate, ThemeAPIDestroy, ProfileView
+from core.views import QuestionAPIUpdate, QuestionAPIDestroy, ThemeAPIList, ThemeAPIUpdate, ThemeAPIDestroy, ProfileView, RegisterView
 from django.conf import settings
 from django.conf.urls.static import static
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-import dj_rest_auth
+
 
 
 urlpatterns = [
@@ -22,4 +22,5 @@ urlpatterns = [
     path('api/themes/', ThemeAPIList.as_view()),
     path('api/themes/<int:pk>/', ThemeAPIUpdate.as_view()),
     path('api/themes/delete/<int:pk>', ThemeAPIDestroy.as_view()),
+    path('api/register/', RegisterView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
